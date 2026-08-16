2384e2c95dba455c4fb341bb918571ce478fee53
Shahzad-Akbar <shad80909@gmail.com>
2026-08-16 13:00:42 +0530

    updated

--- circle/circle.js
+++ circle/circle.js
@@ -2,42 +2,58 @@ const canvas = document.getElementById("canvas")
 const cxt = canvas.getContext('2d')
 let num;
 let circleCount = 0;
-// let r1 = 0;
-// let r2 = 0;
+let circles = [];
 
 
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-canvas.addEventListener("click", function(event){
+canvas.addEventListener("click", function (event) {
 
-    if(circleCount >= 2){
-        cxt.clearRect(0,0, canvas.width, canvas.height);
+    if (circleCount >= 2) {
+        cxt.clearRect(0, 0, canvas.width, canvas.height);
         circleCount = 0
-        // r1=r2=0;
+        circles = [];
 
-    }else{
-         num = Math.floor(Math.random() * 100);            //for random radius
-        //  if(r1 = 0){
-        //     r1=num;
-        //  }else{
-        //     r2=num;
-        //  }
-    drawCircle(event.clientX, event.clientY)
-    circleCount++;
+    } else {
+        num = Math.floor(Math.random() * 100);            //for random radius
+        const circle = {
+            x: event.clientX,
+            y: event.clientY,
+            radius: num
+        };
+
+        circles.push(circle);
+
+        drawCircle(circle.x, circle.y, circle.radius)
+        circleCount++;
     }
 
-    // const differenct = r2-r1;
-    // const radSum = r1+r2;
-   
-    
+    if (circles.length === 2) {
+        checkInterSection(circles[0], circles[1]);
+    }
+
 });
 
-function drawCircle(x, y){
+function drawCircle(x, y, radius) {
     console.log(num);
     cxt.beginPath()
-    cxt.arc(x, y, num, 0, Math.PI * 2);
+    cxt.arc(x, y, radius, 0, Math.PI * 2);
     cxt.stroke()
-    
+
+}
+
+function checkInterSection(circle1, circle2) {
+    dx = circle1.x - circle2.x;
+    dy = circle1.y - circle2.y;
+
+    const distance = Math.sqrt(dx * dx + dy * dy);
+
+    if (distance <= (circle1.radius + circle2.radius)) {
+        console.log("Yes Intersection")
+    } else {
+        console.log("No Intersecting");
+    }
+
 }
