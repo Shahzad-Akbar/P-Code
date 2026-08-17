ab3915eb870482dc03a23186de5e70953aa0266a
Shahzad-Akbar <shad80909@gmail.com>
2026-08-18 00:49:00 +0530

    Reverse done

--- reverse/reverse.js
+++ reverse/reverse.js
@@ -1,5 +1,19 @@
 const button = document.getElementById("btn");
 
+// let str = "shahzad";
+let revStr = "";
+
 button.addEventListener("click", ()=>{
+    let input = document.getElementById("input").value;
+    console.log(input)
+
     console.log("Clicked for reverse")
+    // let revStr = str.split("").reverse().join("");
+    
+    for(let i=input.length-1; i>=0; i--){  
+        revStr += input[i];                       //withoud reverse() method
+    }
+    
+    console.log("After Reversing:", revStr);
+    document.getElementById("result").innerText = "Result :" + revStr;
 })
