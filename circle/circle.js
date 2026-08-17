c2e399ff90a3e555be5dedc0d0d5849a59979316
Shahzad-Akbar <shad80909@gmail.com>
2026-08-17 23:53:05 +0530

    updated

--- circle/circle.js
+++ circle/circle.js
@@ -50,10 +50,78 @@ function checkInterSection(circle1, circle2) {
 
     const distance = Math.sqrt(dx * dx + dy * dy);
 
-    if (distance <= (circle1.radius + circle2.radius)) {
-        console.log("Yes Intersection")
+    if (distance > (circle1.radius + circle2.radius)) {
+        console.log("Not Intersection");
+        console.log("0 Pixel intersection");
     } else {
-        console.log("No Intersecting");
+        console.log("Yes Intersecting");
+
+        // Create temporary canvases
+    const canvas1 = document.createElement("canvas");
+    const canvas2 = document.createElement("canvas");
+
+    canvas1.width = canvas.width;
+    canvas1.height = canvas.height;
+
+    canvas2.width = canvas.width;
+    canvas2.height = canvas.height;
+
+    const ctx1 = canvas1.getContext("2d");
+    const ctx2 = canvas2.getContext("2d");
+
+    // Draw first circle
+    ctx1.beginPath();
+    ctx1.arc(
+        circle1.x,
+        circle1.y,
+        circle1.radius,
+        0,
+        Math.PI * 2
+    );
+    ctx1.fill();
+
+    // Draw second circle
+    ctx2.beginPath();
+    ctx2.arc(
+        circle2.x,
+        circle2.y,
+        circle2.radius,
+        0,
+        Math.PI * 2
+    );
+    ctx2.fill();
+
+    // Get pixel data
+    const pixels1 = ctx1.getImageData(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    ).data;
+
+    const pixels2 = ctx2.getImageData(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    ).data;
+
+    let intersectionPixels = 0;
+
+    // Every pixel has 4 values:
+    // R, G, B, Alpha
+    for (let i = 3; i < pixels1.length; i += 4) {
+
+        const alpha1 = pixels1[i];
+        const alpha2 = pixels2[i];
+
+        // Pixel exists in BOTH circles
+        if (alpha1 > 0 && alpha2 > 0) {
+            intersectionPixels++;
+        }
+    }
+
+    console.log("Intersection pixels:", intersectionPixels)
     }
 
 }
